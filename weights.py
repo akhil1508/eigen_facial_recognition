df6d1713beff6cb3f18b78f3702b68ec8e134ca9
akhil <akhil.potukuchi@gmail.com>
2018-10-31 14:38:30 +0530

    Added documentation for 'weights.py'

--- weights.py
+++ weights.py
@@ -1,9 +1,15 @@
 import numpy as np
 from scipy.spatial import distance
+"""
+  Computes the dot product of the given eigenvector and the matrix of means
+"""
 def return_weight_vector(eigenvector, mean_matrix) :
   #print (np.dot(eigenvector,mean_matrix))
   return np.dot(eigenvector, mean_matrix)
 
+"""
+  Computes the 'distance' between the test_weight and the given weights
+"""
 def distance_(test_weight, weights) :
  # print(x[0])
 #  print("Weights, test weights", weights, test_weight)
